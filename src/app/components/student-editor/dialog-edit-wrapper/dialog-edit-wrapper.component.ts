2c352aa986cb93abaf7b3488ecfb5d4118b9dd54
WolfhoundAD <Loners14@yandex.ru>
2024-03-09 23:45:13 +0300

    Completion of the implementation of the dialog box

--- src/app/components/student-editor/dialog-edit-wrapper/dialog-edit-wrapper.component.ts
+++ src/app/components/student-editor/dialog-edit-wrapper/dialog-edit-wrapper.component.ts
@@ -1,17 +1,28 @@
 import { Component, Inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
 import { Student } from '../../../models/student';
 
 @Component({
   selector: 'app-dialog-edit-wrapper',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, MatInputModule], // Импортируйте необходимые модули
   templateUrl: './dialog-edit-wrapper.component.html',
-  styleUrl: './dialog-edit-wrapper.component.css'
+  styleUrls: ['./dialog-edit-wrapper.component.css']
 })
 export class DialogEditWrapperComponent {
-
+  editingStudent: Student;
   constructor(public dialogRef: MatDialogRef<DialogEditWrapperComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: Student) { }
+    @Inject(MAT_DIALOG_DATA) public data: Student
+    ) {
+      this.editingStudent = new Student();
+      }
 
+     ngOnInit(): void {
+     }
+
+     onNoClick(): void {
+      this.dialogRef.close();
+    }
 }
